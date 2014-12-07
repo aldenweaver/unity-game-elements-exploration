58d05af651fb6c8c400d2dd42661db955278c8a5
aldenweaver <aaldenweaver@gmail.com>
2014-12-06 21:26:44 -0800

    Worked on notifications

---
+++ Assets/Scripts/CoinsGUIAppear.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinsGUIAppear : MonoBehaviour {
+
+	GameObject[] coinsPopUpObjects;
+	
+	// Use this for initialization
+	void Start () {
+		coinsPopUpObjects = GameObject.FindGameObjectsWithTag ("CoinsPopUp");
+		setCoinsPopUpActive(false);
+		
+		Debug.Log ("Initialization of GUIAppear complete.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnTriggerStay (Collider collided) {
+		if(collided.tag == "Player") {
+			setCoinsPopUpActive(true);
+		}
+	}
+	
+	void OnTriggerExit (Collider collided) {
+		if(collided.tag == "Player") {
+			setCoinsPopUpActive(false);
+		}
+	}
+	
+	void setCoinsPopUpActive (bool active) {
+		foreach(GameObject coinsPopUpObject in coinsPopUpObjects) {
+			coinsPopUpObject.SetActive(active);
+		}
+	}
+		
+		
+}
+
---
+++ Assets/Scripts/HorseGUIAppear.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Can abstract this to an interface
+public class HorseGUIAppear : MonoBehaviour {
+
+	GameObject[] horsePopUpObjects;
+
+	// Use this for initialization
+	void Start () {
+		horsePopUpObjects = GameObject.FindGameObjectsWithTag ("HorsePopUp");
+		setHorsePopUpActive(false);
+
+		Debug.Log ("Initialization of GUIAppear complete.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnTriggerStay (Collider collided) {
+		if(collided.tag == "Player") {
+			setHorsePopUpActive(true);
+		}
+	}
+	
+	void OnTriggerExit (Collider collided) {
+		if(collided.tag == "Player") {
+			setHorsePopUpActive(false);
+		}
+	}
+
+	void setHorsePopUpActive (bool active) {
+		foreach(GameObject horsePopUpObject in horsePopUpObjects) {
+			horsePopUpObject.SetActive(active);
+		}
+	}
+
+
+}
